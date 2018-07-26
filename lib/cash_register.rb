f811f8ce109bb5b136fd7404cb643753488958ff
Roberto Garza <robertg4567@hotmail.com>
2018-07-26 12:46:19 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,17 +1,12 @@
 class CashRegister
 
-  attr_accessor :discount
+
 
   def initialize
     @total = 0
 
   end
 
-  def new(discount)
-    @discount = discount
-  end
-  def discount
-    @discount
-  end
+
 
 end
